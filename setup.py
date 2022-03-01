eca0c3988d18685c3dd94993b4494b2954821c37
Huan Chen <hzchenhuan@gmail.com>
2022-03-01 17:01:14 -0500

    First git comment

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smaPy",
+    version="0.0.1",
+    author="Huan Chen",
+    author_email="hchen130@jhu.edu",
+    packages=["smaPy"],
+    description="A Python package implements structrally masked autoencoder, a deep autoencoder for joint decomposing of multiple data sets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CHuanSite/smaPy",
+    license='GPT',
+    python_requires='>=3.6',
+    install_requires=[
+         "numpy",
+         "pandas",
+         "torch",
+         "collections"
+    ]
+)
